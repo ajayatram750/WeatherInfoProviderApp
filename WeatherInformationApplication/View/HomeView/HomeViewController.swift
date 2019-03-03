6216e8c6d5ede422a02508230ea5fc50c2ca6e10
Ajay <ajay.atram@tcs.com>
2019-03-03 13:16:35 +0530

    Remove dead code from Home view

--- WeatherInformationApplication/View/HomeView/HomeViewController.swift
+++ WeatherInformationApplication/View/HomeView/HomeViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import CoreLocation
 
-class HomeViewController: UIViewController,UITextFieldDelegate {
+class HomeViewController: UIViewController {
     
     //MARK:- IBoutlet declarations
-    
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var errorLabelForLatLon: UILabel!
     @IBOutlet weak var getLatAndLongButton: UIButton!
@@ -20,7 +19,6 @@ class HomeViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var longitudeTextField: UITextField!
     
     //MARK:- Class life cycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,10 +36,7 @@ class HomeViewController: UIViewController,UITextFieldDelegate {
     fileprivate func initialUpdateUI() {
         latitudeTextField.text = ""
         longitudeTextField.text = ""
-        
-        latitudeTextField.delegate = self
-        longitudeTextField.delegate = self
-        
+
         backgroundView.layer.borderWidth = 1
         backgroundView.layer.borderColor = UIColor.black.cgColor
         
@@ -49,124 +44,84 @@ class HomeViewController: UIViewController,UITextFieldDelegate {
         getLatAndLongButton.layer.borderColor = UIColor.black.cgColor
     }
     
-    //MARK:- UITextfield delegate method
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let doubleValueAcceptableSet = NSCharacterSet(charactersIn:"0123456789.").inverted
-        let compSepByCharInSet = string.components(separatedBy: doubleValueAcceptableSet)
-        let numberFiltered = compSepByCharInSet.joined(separator: "")
-        return string == numberFiltered
-    }
-    
     //MARK:- Button Actions
     
-    /*
-     Method Name : getWeatherDetailsButtonClicked button action
-     Purpose     : It call the Weather Details ViewController
-     Paramater   : Any
-     Return      : None
-     */
+    //This method is used for calling the Weather Details ViewController if user get Lat & Log from Geo
     @IBAction func getWeatherDetailsButtonClicked(_ sender: Any) {
         
-        if(latitudeTextField.text == "" && longitudeTextField.text == ""){
-            self.showAlert(title: ErrorMessages.LATLONG_INPUT_MISSING, message: "")
-        }else if(latitudeTextField.text == ""){
+        guard let txtLat = latitudeTextField.text, txtLat.count > 0 else {
             self.showAlert(title: ErrorMessages.LATITUDE_INPUT_MISSING, message: "")
-        }else if(longitudeTextField.text == ""){
-            self.showAlert(title: ErrorMessages.LONGITUDE_INPUT_MISSING, message: "")
-        }else{
-            let myVC = storyboard?.instantiateViewController(withIdentifier: "WeatherDetailsViewController") as! WeatherDetailsViewController
-            myVC.latitude = latitudeTextField.text!
-            myVC.longitude = longitudeTextField.text!
-            navigationController?.pushViewController(myVC, animated: true)
+            return
         }
+        guard let txtLong = longitudeTextField.text, txtLong.count > 0 else {
+            self.showAlert(title: ErrorMessages.LONGITUDE_INPUT_MISSING, message: "")
+            return
+        }
+        guard let myVC = storyboard?.instantiateViewController(withIdentifier: "WeatherDetailsViewController") as? WeatherDetailsViewController else {
+            return
+        }
+        myVC.latitude = txtLat
+        myVC.longitude = txtLong
+        navigationController?.pushViewController(myVC, animated: true)
     }
     
-    /*
-     Method Name : getLatAndLongFromCity button action
-     Purpose     : Its simply call the popup for input city name and provides the latitude & longitude
-     Paramater   : Any
-     Return      : None
-     */
+    // Its simply call the popup for input city name and provides the latitude & longitude
     @IBAction func getLatAndLongFromCity(_ sender: Any) {
         self.promptForCityInput()
     }
     
-    /*
-     Method Name : getLatAndLong
-     Purpose     : Using City name, Its provide the latitude & longitude cordinates for that city
-     Paramater   : address
-     Return      : None
-     */
+    
+    // Using City name, Its provide the latitude & longitude cordinates for that city
     func getLatAndLong(address:String){
         
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(address) {
             placemarks, error in
-            let placemark = placemarks?.first
-            let lat = placemark?.location?.coordinate.latitude
-            let lon = placemark?.location?.coordinate.longitude
-            self.errorLabelForLatLon.isHidden = true
-            
-            if let latitude = lat{
-                self.latitudeTextField.text = String(latitude)
-            }else{
-                self.errorLabelForLatLon.isHidden = false
-                self.errorLabelForLatLon.text = ErrorMessages.ERROR_TO_GET_LATLON
-            }
-            
-            if let longitude = lon{
-                self.longitudeTextField.text = String(longitude)
-            }else{
-                self.errorLabelForLatLon.isHidden = false
-                self.errorLabelForLatLon.text = ErrorMessages.ERROR_TO_GET_LATLON
-            }
+            self.handleGeocodeAddressResponse(placemarks)
         }
     }
     
-    /*
-     Method Name : promptForCityInput
-     Purpose     : Enter City name, Its provide the latitude & longitude cordinates for that city
-     Paramater   : None
-     Return      : None
-     */
+    func handleGeocodeAddressResponse(_ placemarks: [CLPlacemark]?) {
+        
+        self.errorLabelForLatLon.isHidden = true
+        if let latitude = placemarks?.first?.location?.coordinate.latitude {
+            self.latitudeTextField.text = String(latitude)
+        } else {
+            self.errorLabelForLatLon.isHidden = false
+            self.errorLabelForLatLon.text = ErrorMessages.ERROR_TO_GET_LATLON
+        }
+        if let longitude = placemarks?.first?.location?.coordinate.longitude {
+            self.longitudeTextField.text = String(longitude)
+        } else {
+            self.errorLabelForLatLon.isHidden = false
+            self.errorLabelForLatLon.text = ErrorMessages.ERROR_TO_GET_LATLON
+        }
+    }
+    
+    //This method is used to enter city or any place name, It's provide the latitude & longitude cordinates for that city
     func promptForCityInput() {
-        
         let alert = UIAlertController(title: GeneralText.CITY_NAME_REQUEST, message: "", preferredStyle: .alert)
-        
         alert.addTextField { (textField) in
             textField.placeholder = GeneralText.CITY_NAME_REQUEST
         }
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0]
-            
-            
-            if let cityName = textField?.text{
-                
-                if(cityName != ""){
-                    self.getLatAndLong(address:cityName)
-                }
+            if let textFields = alert?.textFields, let cityName = textFields[0].text, cityName.count > 0 {
+                self.getLatAndLong(address:cityName)
             }
         }))
-        
         self.present(alert, animated: true, completion: nil)
     }
     
-    /*
-     Method Name : touchesBegan
-     Purpose     : Use to make the view or any subview that is the first responder resign
-     Paramater   : UITouch & touch event
-     Return      : None
-     */
+    // Use to make the view or any subview that is the first responder resign
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         view.endEditing(true)
         super.touchesBegan(touches, with: event)
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension HomeViewController: UITextFieldDelegate {
+    //MARK:- UITextfield delegate method to allow only numeric
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return string == string.components(separatedBy: NSCharacterSet(charactersIn:"0123456789.").inverted).joined(separator: "")
     }
-    
 }
